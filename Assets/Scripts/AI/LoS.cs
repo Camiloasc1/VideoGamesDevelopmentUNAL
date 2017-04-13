69311020778720c992a5a602114dd2659e849337
Camilo Sanchez <camiloasc1@gmail.com>
2017-04-13 15:49:55 -0500

    LoS events added

--- Assets/Scripts/AI/LoS.cs
+++ Assets/Scripts/AI/LoS.cs
@@ -6,18 +6,20 @@ namespace AI
 {
     public class LoS : MonoBehaviour
     {
-        [Tooltip("The field of view in degrees")] [Range(0, 360)] public float FoV = 30f;
-        [Tooltip("The eyes offset")] public Vector3 EyesOffset = new Vector3(0, 1.42f, 0);
-        [Tooltip("The maximum view distance")] public float ViewDistance = 10;
-        [Tooltip("The maximum hear distance")] public float HearDistance = 2.5f;
-        [Tooltip("The list of GOs to look for")] public List<GameObject> LookFor;
+        [Tooltip("The field of view in degrees")] [Range(0, 360)] public float fieldOfView = 30f;
+        [Tooltip("The eyes offset")] public Vector3 eyesOffset = new Vector3(0, 1.42f, 0);
+        [Tooltip("The maximum view distance")] public float viewDistance = 10;
+        [Tooltip("The maximum hear distance")] public float hearDistance = 2.5f;
+        [Tooltip("The list of GOs to look for")] public List<GameObject> targets;
+
+        private readonly Dictionary<GameObject, bool> targetWasOnView = new Dictionary<GameObject, bool>();
 
         private void Reset()
         {
             var player = GameObject.FindGameObjectWithTag("Player");
             if (player != null)
             {
-                LookFor.Add(player);
+                targets.Add(player);
             }
         }
 
@@ -29,32 +31,57 @@ namespace AI
         {
         }
 
+        private void OnEnable()
+        {
+            foreach (var target in targets)
+            {
+                targetWasOnView[target] = false;
+            }
+        }
+
         private void Update()
         {
-            foreach (var target in LookFor)
+            foreach (var target in targets)
             {
-                CanSeeTarget(target);
+                var canSee = CanSeeTarget(target);
+                if (!targetWasOnView[target] && canSee)
+                {
+                    SendMessage("OnLoSEnter", target, SendMessageOptions.DontRequireReceiver);
+                }
+                else if (targetWasOnView[target] && canSee)
+                {
+                    SendMessage("OnLoSStay", target, SendMessageOptions.DontRequireReceiver);
+                }
+                else if (targetWasOnView[target] && !canSee)
+                {
+                    SendMessage("OnLoSExit", target, SendMessageOptions.DontRequireReceiver);
+                }
+                targetWasOnView[target] = canSee;
             }
         }
 
         private void OnDrawGizmos()
         {
             Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(transform.position, HearDistance);
-            Gizmos.DrawWireSphere(transform.position, ViewDistance);
+            Gizmos.DrawWireSphere(transform.position, hearDistance);
+            Gizmos.DrawWireSphere(transform.position, viewDistance);
             Gizmos.DrawLine(transform.position,
-                transform.position + Quaternion.Euler(0, FoV, 0) * transform.forward * ViewDistance);
+                transform.position + Quaternion.Euler(0, fieldOfView, 0) * transform.forward * viewDistance);
             Gizmos.DrawLine(transform.position,
-                transform.position + Quaternion.Euler(0, -FoV, 0) * transform.forward * ViewDistance);
+                transform.position + Quaternion.Euler(0, -fieldOfView, 0) * transform.forward * viewDistance);
+            Gizmos.DrawLine(transform.position,
+                transform.position + Quaternion.Euler(fieldOfView, 0, 0) * transform.forward * viewDistance);
+            Gizmos.DrawLine(transform.position,
+                transform.position + Quaternion.Euler(-fieldOfView, 0, 0) * transform.forward * viewDistance);
 
             Gizmos.color = Color.green;
             if (Application.isPlaying)
             {
-                foreach (var target in LookFor)
+                foreach (var target in targets)
                 {
                     if (CanSeeTarget(target))
                     {
-                        Gizmos.DrawLine(transform.position + EyesOffset, target.transform.position + EyesOffset);
+                        Gizmos.DrawLine(transform.position + eyesOffset, target.transform.position + eyesOffset);
                     }
                 }
             }
@@ -66,20 +93,20 @@ namespace AI
             var toTargetMagnitude = toTarget.magnitude; // Avoid the property's internal sqrt each time
 
             // Is near enough
-            if (toTargetMagnitude < HearDistance)
+            if (toTargetMagnitude < hearDistance)
             {
                 // Has a clear view of the target
                 return RaycastToTarget(toTarget, target);
             }
 
             // Is far enough
-            if (toTargetMagnitude > ViewDistance)
+            if (toTargetMagnitude > viewDistance)
             {
                 return false;
             }
 
             // Is inside the FoV
-            if (Vector3.Angle(transform.forward, toTarget) > FoV)
+            if (Vector3.Angle(transform.forward, toTarget) > fieldOfView)
             {
                 return false;
             }
@@ -91,7 +118,7 @@ namespace AI
         private bool RaycastToTarget(Vector3 toTarget, GameObject target)
         {
             RaycastHit hitInfo;
-            if (Physics.Raycast(transform.position + EyesOffset, toTarget, out hitInfo, toTarget.magnitude))
+            if (Physics.Raycast(transform.position + eyesOffset, toTarget, out hitInfo, toTarget.magnitude))
             {
                 return hitInfo.transform.gameObject == target;
             }

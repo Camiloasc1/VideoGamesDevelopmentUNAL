9477e75e63f8688c615d52c00d4a91163258bf19
Camilo Sanchez <camiloasc1@gmail.com>
2017-04-13 18:40:00 -0500

    Patrol and Chase behavior

--- Assets/Scripts/AI/Patrol/AIPatrolUnitController.cs
+++ Assets/Scripts/AI/Patrol/AIPatrolUnitController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.AI;
 using UnityStandardAssets.Characters.ThirdPerson;
 
@@ -7,18 +8,39 @@ namespace AI.Patrol
     [RequireComponent(typeof(AICharacterControl))]
     public class AIPatrolUnitController : MonoBehaviour
     {
+        private NavMeshAgent navAgent;
         private AICharacterControl characterControl;
-        private Transform oldTarget;
+
+        private Transform patrolTarget;
+        private Transform chaseTarget;
+        private AIPatrolUnitStates state;
 
         private void Awake()
         {
+            navAgent = GetComponentInChildren<NavMeshAgent>();
             characterControl = GetComponent<AICharacterControl>();
+        }
+
+        private void Start()
+        {
+            patrolTarget = characterControl.target;
+        }
+
+        private void Update()
+        {
+            // Update the current state and act accordingly
+            if (UpdateState())
+            {
+                OnStateChange();
+            }
+
+            // Execute the state
+            ExecuteState();
         }
 
         private void OnLoSEnter(Transform target)
         {
-            oldTarget = characterControl.target;
-            characterControl.target = target;
+            chaseTarget = target;
         }
 
         private void OnLoSStay(Transform target)
@@ -27,7 +49,83 @@ namespace AI.Patrol
 
         private void OnLoSExit(Transform target)
         {
-            characterControl.target = oldTarget;
+            chaseTarget = null;
         }
+
+        private bool UpdateState()
+        {
+            switch (state)
+            {
+                case AIPatrolUnitStates.Patrol:
+                    if (chaseTarget != null)
+                    {
+                        state = AIPatrolUnitStates.Chasing;
+                        return true;
+                    }
+                    break;
+                case AIPatrolUnitStates.Chasing:
+                    if (chaseTarget == null)
+                    {
+                        state = AIPatrolUnitStates.Lost;
+                        return true;
+                    }
+                    break;
+                case AIPatrolUnitStates.Lost:
+                    if (chaseTarget != null)
+                    {
+                        state = AIPatrolUnitStates.Chasing;
+                        return true;
+                    }
+                    else if (navAgent.remainingDistance < navAgent.stoppingDistance)
+                    {
+                        state = AIPatrolUnitStates.Patrol;
+                        return true;
+                    }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            return false;
+        }
+
+        private void OnStateChange()
+        {
+            switch (state)
+            {
+                case AIPatrolUnitStates.Patrol:
+                    characterControl.target = patrolTarget;
+                    break;
+                case AIPatrolUnitStates.Chasing:
+                    characterControl.target = chaseTarget;
+                    break;
+                case AIPatrolUnitStates.Lost:
+                    characterControl.target = null;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private void ExecuteState()
+        {
+            switch (state)
+            {
+                case AIPatrolUnitStates.Patrol:
+                    break;
+                case AIPatrolUnitStates.Chasing:
+                    break;
+                case AIPatrolUnitStates.Lost:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+
+    public enum AIPatrolUnitStates
+    {
+        Patrol,
+        Chasing,
+        Lost
     }
 }

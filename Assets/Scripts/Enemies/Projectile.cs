92ba4d8e5d11225b0c5e15d10fa3b22a74082291
Camilo Sanchez <camiloasc1@gmail.com>
2017-04-14 17:41:48 -0500

    Basic projectile

---
+++ Assets/Scripts/Enemies/Projectile.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Enemies
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class Projectile : MonoBehaviour
+    {
+        [Tooltip("The initial velocity")] public float velocity = 1.0f;
+        [Tooltip("The initial life span")] public float lifeSpan = 5.0f;
+
+        private void Start()
+        {
+            GetComponent<Rigidbody>().velocity = transform.forward * velocity;
+            Destroy(gameObject, lifeSpan);
+        }
+    }
+}

a6975b42ca842d9a2476e7cba285ab60a0bec528
Camilo Sanchez <camiloasc1@gmail.com>
2017-04-30 20:59:42 -0500

    Player health and damage

---
+++ Assets/Scripts/Player/PlayerCharacter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class PlayerCharacter : MonoBehaviour
+    {
+        [Tooltip("The player's max health")] public float maxHealth = 5f;
+
+        public float Health { get; private set; }
+
+        // Use this for initialization
+        private void Start()
+        {
+            Health = maxHealth;
+        }
+
+        public bool Damage(float damage)
+        {
+            Health -= damage;
+            print(Health);
+            if (Health <= 0f)
+            {
+                Die();
+                return true;
+            }
+            return false;
+        }
+
+        private void Die()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

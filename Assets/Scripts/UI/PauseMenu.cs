4a63d02cc288113f0ee643b3995ce76991dd5ebc
DanielNavas <dfnavasm@unal.edu.co>
2017-04-30 19:57:34 -0500

    Pause menu working

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+	public Transform pausePanel;
+	public Transform optionsPanel;
+
+	public void toggleOptions ()
+	{
+		if (optionsPanel.gameObject.activeInHierarchy == false) {
+			pausePanel.gameObject.SetActive (false);
+			optionsPanel.gameObject.SetActive (true);
+		} else {
+			optionsPanel.gameObject.SetActive (false);
+			pausePanel.gameObject.SetActive (true);
+		}
+	}
+}

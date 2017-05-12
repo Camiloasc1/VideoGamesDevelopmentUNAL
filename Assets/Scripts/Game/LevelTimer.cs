1e6a2d31ecb2b322ab7a81fdc6dd2a4d65c4bab6
Camilo Sanchez <camiloasc1@gmail.com>
2017-05-11 21:01:23 -0500

    Basic time limit class

---
+++ Assets/Scripts/Game/LevelTimer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class LevelTimer : MonoBehaviour
+    {
+        [Tooltip("The level's time limit")] public float timeLimit;
+
+        public float TimeLeft { get; private set; }
+
+        private void Start()
+        {
+            TimeLeft = timeLimit;
+        }
+
+        private void Update()
+        {
+            TimeLeft -= Time.deltaTime;
+        }
+    }
+}

ed16ac95a4414b670a4b521618e6f69ce699e3b5
Camilo Sanchez <camiloasc1@gmail.com>
2017-05-12 11:40:39 -0500

    RemainingTime script

---
+++ Assets/Scripts/UI/RemainingTime.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Game;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    [RequireComponent(typeof(Text))]
+    public class RemainingTime : MonoBehaviour
+    {
+        private LevelTimer levelTimer;
+        private Text timeText;
+
+        private void Awake()
+        {
+            var gameController = GameObject.FindGameObjectWithTag("GameController");
+            if (!gameController)
+            {
+                throw new ArgumentNullException("gameController", "GameController not found");
+            }
+            levelTimer = gameController.GetComponent<LevelTimer>();
+            if (!levelTimer)
+            {
+                throw new ArgumentNullException("levelTimer", "levelTimer not found in GameController");
+            }
+            timeText = GetComponent<Text>();
+        }
+
+        private void Update()
+        {
+            if (!levelTimer)
+            {
+                return;
+            }
+            timeText.text = levelTimer.TimeString;
+        }
+    }
+}

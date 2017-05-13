195f36ec14fabdcb5daf407a7f7c193b7b799256
Camilo Sanchez <camiloasc1@gmail.com>
2017-05-12 20:14:23 -0500

    Load PlayerPrefs in the Sliders

---
+++ Assets/Scripts/UI/Settings/LoadSliderPref.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Settings
+{
+    [RequireComponent(typeof(Slider))]
+    public class LoadSliderPref : MonoBehaviour
+    {
+        public string pref;
+
+        private void Start()
+        {
+            if (pref.Length == 0)
+            {
+                return;
+            }
+            GetComponent<Slider>().value = PlayerPrefs.GetFloat(pref);
+        }
+    }
+}

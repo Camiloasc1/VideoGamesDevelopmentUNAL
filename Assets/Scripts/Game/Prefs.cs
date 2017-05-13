5fc8f0d8213965e4e24fe54c76f1158a61ba5447
Camilo Sanchez <camiloasc1@gmail.com>
2017-05-12 20:14:00 -0500

    PlayerPrefs

---
+++ Assets/Scripts/Game/Prefs.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Game
+{
+    public class Prefs : MonoBehaviour
+    {
+        public AudioMixer audioMixer;
+
+        private void Start()
+        {
+            ReloadPlayerPrefs();
+        }
+
+        private void ReloadPlayerPrefs()
+        {
+            audioMixer.SetFloat("FXVolume", PlayerPrefs.GetFloat("FXVolume"));
+            audioMixer.SetFloat("MusicVolume", PlayerPrefs.GetFloat("MusicVolume"));
+        }
+
+        public void SetFXVolume(float value)
+        {
+            PlayerPrefs.SetFloat("FXVolume", value);
+            audioMixer.SetFloat("FXVolume", value);
+        }
+
+        public void SetMusicVolume(float value)
+        {
+            PlayerPrefs.SetFloat("MusicVolume", value);
+            audioMixer.SetFloat("MusicVolume", value);
+        }
+    }
+}

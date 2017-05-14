5957dd8320d5456886935038d05d6bf3d51b45ba
Camilo Sanchez <camiloasc1@gmail.com>
2017-05-14 18:46:10 -0500

    Damage and die sounds

--- Assets/Scripts/Player/PlayerCharacter.cs
+++ Assets/Scripts/Player/PlayerCharacter.cs
@@ -3,11 +3,16 @@ using UnityEngine;
 
 namespace Player
 {
+    [RequireComponent(typeof(AudioSource))]
     public class PlayerCharacter : MonoBehaviour
     {
         [Tooltip("The player's max health")] public float maxHealth = 5f;
+        public PlayerSounds playerSounds;
+
+        private AudioSource audioSource;
 
         public delegate void OnPlayerRecieveDamage_(float damage);
+
         public delegate void OnPlayerDeath_();
 
         public event OnPlayerRecieveDamage_ OnPlayerRecieveDamage = delegate { };
@@ -20,9 +25,35 @@ namespace Player
             get { return Health / maxHealth; }
         }
 
+        private void Awake()
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+
         private void Start()
         {
             Health = maxHealth;
+        }
+
+        private void PlaySound(AudioClip clip)
+        {
+            if (!clip)
+            {
+                return;
+            }
+
+            audioSource.clip = clip;
+            audioSource.Play();
+        }
+
+        private void PlaySound(AudioClip[] clips)
+        {
+            if (clips.Length == 0)
+            {
+                return;
+            }
+
+            PlaySound(clips[Random.Range(0, clips.Length)]);
         }
 
         public void Damage(float damage)
@@ -30,10 +61,12 @@ namespace Player
             if (Health > 0f)
             {
                 Health -= damage;
+                PlaySound(playerSounds.damage);
                 OnPlayerRecieveDamage(damage);
                 if (Health <= 0f)
                 {
                     Die();
+                    PlaySound(playerSounds.death);
                     OnPlayerDeath();
                 }
             }
@@ -43,5 +76,14 @@ namespace Player
         {
             GameController.GameMode.GameOver();
         }
+    }
+
+    [System.Serializable]
+    public struct PlayerSounds
+    {
+        [Tooltip("Hurt sounds")] public AudioClip[] damage;
+        [Tooltip("Death sounds")] public AudioClip[] death;
+        [Tooltip("Foot steps sounds")] public AudioClip[] footSteps;
+        [Tooltip("Low health sound")] public AudioClip lowHealth;
     }
 }

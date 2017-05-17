7bfdb77c4f2dca5dbb3a0df0bc7ae99b80ee476c
Camilo Sanchez <camiloasc1@gmail.com>
2017-05-16 20:55:47 -0500

    Pay random sound script

---
+++ Assets/Scripts/Audio/PlayRandomSound.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Audio
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class PlayRandomSound : MonoBehaviour
+    {
+        public bool random;
+        public Vector2 pauseBetweenClips;
+        public AudioClip[] clips;
+
+        private AudioSource audioSource;
+        private int current;
+        private Coroutine player;
+
+        private void Awake()
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        private void OnEnable()
+        {
+            player = StartCoroutine(PlayNext());
+        }
+
+        private void OnDisable()
+        {
+            StopCoroutine(player);
+        }
+
+        private IEnumerator PlayNext()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(Random.Range(pauseBetweenClips.x, pauseBetweenClips.y));
+
+                if (random)
+                {
+                    current = Random.Range(0, clips.Length);
+                }
+                else
+                {
+                    current = ++current % clips.Length;
+                }
+
+                audioSource.clip = clips[current];
+                audioSource.Play();
+
+                yield return new WaitForSeconds(clips[current].length);
+            }
+        }
+    }
+}

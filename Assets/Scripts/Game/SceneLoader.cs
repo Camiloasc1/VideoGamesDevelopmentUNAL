cf0435847781d6e8b7c91467ec725c78f8f2da4e
Camilo Sanchez <camiloasc1@gmail.com>
2017-05-17 21:04:26 -0500

    Basic scene loader

---
+++ Assets/Scripts/Game/SceneLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Game
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        public void LoadMainMenu()
+        {
+            LoadScene(0);
+        }
+
+        public void LoadMainScene()
+        {
+            LoadScene(1);
+        }
+
+        public void LoadScene(int scene)
+        {
+            StartCoroutine(LoadAsync(scene));
+        }
+
+        private static IEnumerator LoadAsync(int scene)
+        {
+            var loading = SceneManager.LoadSceneAsync(scene);
+            yield return loading;
+        }
+    }
+}
